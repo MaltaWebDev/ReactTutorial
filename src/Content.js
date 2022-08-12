4d6e88369c04298e54c4995ef2a30f9201511cfd
MaltaWebDev <MaltaWebDev@gmail.com>
2022-08-12 05:15:07 +0200

    added click events

--- src/Content.js
+++ src/Content.js
@@ -7,11 +7,26 @@ const Content = () => {
     return names[int];
 }
 
+  const handleClick = () => {
+    console.log('you clicked button1')
+  }
+
+  const handleClick2 = (name) => {
+    console.log(`you clicked ${name}`)
+  }
+
+  const handleClick3 = (event1) => {
+    console.log(event1.target.innerText)
+  }
+ 
   return (
     <main>
-        <p>
+        <p onDoubleClick={handleClick}>
         Hello {handleNameChange()}!
         </p>
+        <button onClick={handleClick}>Click Me</button>
+        <button onClick={() => handleClick2('Dave')}>Click Me</button>
+        <button onClick={(event1) => handleClick3(event1)}>Click Me</button>
     </main>
   )
 }
